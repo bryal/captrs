ac46d716e862c9f34f6cb4fef00463852efcf246
TommyGymer <tommygymer@gmail.com>
2023-05-15 12:53:57 +0100

    Capture test

--- src/lib.rs
+++ src/lib.rs
@@ -219,3 +219,21 @@ impl Capturer {
         self.image.as_deref()
     }
 }
+
+#[cfg(test)]
+mod captrs_tests {
+    use super::*;
+
+    #[test]
+    fn test_capture() {
+        let mut capturer = Capturer::new(1).unwrap();
+
+        let (w, h) = capturer.geometry();
+
+        let frame = capturer.capture_frame_components().unwrap();
+
+        // check that the capture is the correct size
+        // should be width * height * $ (RGBA)
+        assert_eq!((w * h * 4) as usize, frame.len())
+    }
+}
